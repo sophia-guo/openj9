f54fbca568592d101c5203a59afca3ecb84cf074
Sophia Guo <sophiag@ca.ibm.com>
2019-11-07 12:06:04 -0500

    update former commit
    Signed-off-by: Sophia Guo <sophiag@ca.ibm.com>

--- test/functional/CacheManagement/src/tests/sharedclasses/options/TestPersistentCacheMoving01.java
+++ test/functional/CacheManagement/src/tests/sharedclasses/options/TestPersistentCacheMoving01.java
@@ -39,10 +39,9 @@ public class TestPersistentCacheMoving01 extends TestUtils {
 		  return;
 	  }
 	  String currentCacheDir = getCacheDir();
-
 	  if ( null == currentCacheDir && isOpenJ9() ) {
 		  // Persistent cachefile without groupaccess are generated under HOME directory
-		  // Move a file from one filesystem to another may not succeed. 
+		  // The current folder is under /tmp. Move a file from HOME filesystem to /tmp may not succeed. 
 		return;
 	  }
 	  
@@ -56,8 +55,7 @@ public class TestPersistentCacheMoving01 extends TestUtils {
 		  String loc = getCacheFileLocationForPersistentCache("Foo");
 		  File f = new File(loc);
 		  File fHidden = new File(tmpdir+File.separator+getCacheFileName("Foo", true));
-		//  System.out.println(loc);
-		//  System.out.println(fHidden);
+
 		  if (!f.renameTo(fHidden)) fail("Could not rename the control file to be hidden");
 		  setCacheDir(tmpdir);
 		  listAllCaches();
